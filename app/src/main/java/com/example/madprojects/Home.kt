638b5c670155b552d0fb336ee7260d3dafc3915c
Dinuka <it21166624@my.sliit.lk>
2023-05-14 13:17:41 +0530

    home changes

---
+++ app/src/main/java/com/example/madprojects/Home.kt
@@ -0,0 +1,23 @@
+package com.example.madprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.madproject.R
+import com.google.firebase.database.FirebaseDatabase
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+
+    }
+}
+
+//        val button = findViewById<Button>(R.id.continuebtn)
+//        button.setOnClickListener {
+//
+//        }
+
