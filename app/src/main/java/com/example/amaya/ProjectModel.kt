c6e7002ef13107c3acdbfbbd876fe2a566ab9879
Rathnayake R.M.D.A <it21171192@my.sliit.lk>
2023-05-28 20:51:56 -0700

    add Project Model

---
+++ app/src/main/java/com/example/amaya/ProjectModel.kt
@@ -0,0 +1,8 @@
+package com.example.amaya
+
+data class ProjectModel(
+    val projectId : String? = null,
+    val projectName : String? = null,
+    val organizationName : String? = null,
+    val description : String? = null
+)
