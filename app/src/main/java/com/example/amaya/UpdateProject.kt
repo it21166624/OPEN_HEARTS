7155dbaaef33c1ed5f80d49de6dca174b6904c58
Rathnayake R.M.D.A <it21171192@my.sliit.lk>
2023-05-28 20:50:48 -0700

    Add project Update page and functionality

---
+++ app/src/main/java/com/example/amaya/UpdateProject.kt
@@ -0,0 +1,11 @@
+package com.example.amaya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_project)
+    }
+}
